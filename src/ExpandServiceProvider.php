524a45f6e91b6e1927b136970c815d8eaa4db952
twilroad <269044570@qq.com>
2017-09-19 11:59:55 +0800

    add base classes for expand

---
+++ src/ExpandServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <heshudong@ibenchu.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-09-19 10:37
+ */
+namespace Notadd\Composer;
+
+use Notadd\Foundation\Expand\Abstracts\Expand;
+
+/**
+ * Class ExpandServiceProvider.
+ */
+class ExpandServiceProvider extends Expand
+{
+}
