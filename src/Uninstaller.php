f6d04c024ff084334d6378bc18a3cd8bd8668095
twilroad <269044570@qq.com>
2017-10-05 23:56:03 +0800

    add uninstaller

---
+++ src/Uninstaller.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <heshudong@ibenchu.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-10-05 23:55
+ */
+namespace Notadd\Composer;
+
+/**
+ * Class Uninstaller.
+ */
+class Uninstaller extends \Notadd\Foundation\Extension\Abstracts\Uninstaller
+{
+    /**
+     * @return true
+     */
+    public function handle()
+    {
+        return true;
+    }
+}
